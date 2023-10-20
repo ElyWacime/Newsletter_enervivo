82f708b9efdf6e9f0c12609e19aba1ef6b46eb21
Abdellatif231 <abdomaouni404@gmail.com>
2023-10-20 18:59:57 +0100

    switch

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 let container = document.getElementById("article");
 
 
-const response = fetch("https://api.enervivo.fr/get")
+fetch("https://api.enervivo.fr/get")
     .then(res => res.json())
     .then(data => {
-        data.map(obj => {
-            console.log(obj)
-            const arrOfObj = Object.entries(obj);
-            arrOfObj.map(([key, value]) => {
-                console.log(key)
-                const valueArr = Object.entries(value);
-                valueArr.map(([key, value]) => {
-                    const h2 = document.createElement('h2');
-                    h2.innerHTML = `<h2>${key}</h2>`;
-                    container.append(h2);
-                    const valueArr2 = Object.entries(value);
-                    valueArr2.map(([key, value]) => {
-                        container.append(display(value))
+        for (obj of data) {
+            if (obj.hasOwnProperty("WHAT THE FUCK")){
+                const arrOfObj = Object.entries(obj);
+                arrOfObj.map(([key, value]) => {
+                    const valueArr = Object.entries(value);
+                    valueArr.map(([key, value]) => {
+                        const h2 = document.createElement('h2');
+                        h2.innerHTML = `<h2>${key}</h2>`;
+                        container.append(h2);
+                        const valueArr2 = Object.entries(value);
+                        valueArr2.map(([key, value]) => {
+                            container.append(display(value))
+                        })
                     })
                 })
-            })
-        })
+            }
+        }
         }
     )
-
-console.log(response)
 
 function display({title, image, description}){
     let article = document.createElement('div');
